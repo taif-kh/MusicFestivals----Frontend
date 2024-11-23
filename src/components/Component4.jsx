5111232c01d83d4ab579196794afceaed8142b05
TaiKha7 <154641318+TaiKha7@users.noreply.github.com>
2024-11-23 14:07:14 +0100

    Replace "Technologied used" with Models and Controllers

--- src/components/Component4.jsx
+++ src/components/Component4.jsx
@@ -10,28 +10,39 @@ const Component4 = forwardRef(({ component5Ref }, ref) => {
   return (
     <div
       ref={ref}
-      className="h-screen flex flex-col justify-center items-center w-screen  relative overflow-x-hidden"
+      className="h-screen flex flex-col justify-center items-start w-screen  pl-[50px] pt-[30px]  relative overflow-hidden"
     >
       <div className=' pb-16'>
-        <h1 className='text-[67px] '>Technologies used</h1>
-        <div className='pl-8 font-medium list-disc'>
- <div className='flex items-center justify-start gap-x-5 '>        <h1>React.js </h1>         <img src="/react.svg" className="invert w-14 h-14" alt="React Logo" />
+        {/* <div className='pl-8 font-medium list-disc flex gap-x-16 w-[1300px] items-center justify-center '>
+ <div className='flex items-center justify-start gap-x-3 '>        <h2>React.js </h2>         <img src="/react.svg" className="invert w-14 h-14" alt="React Logo" />
 </div>
-        <div className='flex items-center justify-start gap-x-5'>        <h1>PostgreSQL </h1>         <img src="/postgre.svg" className="invert w-14 h-14" alt="React Logo" />
+        <div className='flex items-center justify-start gap-x-3'>        <h2>PostgreSQL </h2>         <img src="/postgre.svg" className="invert w-14 h-14" alt="React Logo" />
         </div>
-        <div className='flex items-center justify-start gap-x-5'>        <h1>Express.js </h1>         <img src="/expressOne.svg" className="invert w-14 h-16" alt="React Logo" />
+        <div className='flex items-center justify-start gap-x-3'>        <h2>Express.js </h2>         <img src="/expressOne.svg" className="invert w-14 h-16" alt="React Logo" />
         </div>
-        <div className='flex items-center justify-start gap-x-5'>        <h1>Node.js </h1>         <img src="/nodejs.svg" className="invert w-14 h-14" alt="React Logo" />
+        <div className='flex items-center justify-start gap-x-3'>        <h2>Node.js </h2>         <img src="/nodejs.svg" className="invert w-14 h-14" alt="React Logo" />
         </div>
         </div>
-
+        <br /> */}
         <h1 className='text-[67px] '>Controllers used</h1>
         <ol className='pl-12 list-disc flex gap-x-24'>
         <li>Events' Controller</li>
           <li>Users' Controller</li>
           <li>Key's Controller</li>
         </ol>
+        <br />
 
+
+        <h1 className='text-[67px] '>Models</h1>
+        <ol className='pl-12 list-disc flex gap-x-[200px] '>
+        <li>Events' model</li>
+          <li>Users' model</li>
+        </ol>
+        <br />
+        <div className='flex gap-x-12 pl-12 '>
+        <img src="/group2.png" className='h-[300px] ' />
+        <img src="/group3.png" className='h-[300px] ' />
+        </div>
 
         {/* <img
           src="/javascript-155-svgrepo-com.svg"
@@ -40,7 +51,7 @@ const Component4 = forwardRef(({ component5Ref }, ref) => {
         /> */}
       </div>
       
-      <button onClick={handleScroll} className="absolute bottom-0">
+      <button onClick={handleScroll} className="absolute right-[700px] bottom-0">
         <img src="/down.png" alt="Scroll Down" />
       </button>
     </div>
